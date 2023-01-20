bc3248c21cbf0a365c94feed747d02c0b898afe5
Atthaset <iaut2545@gmail.com>
2023-01-20 18:22:10 +0700

    “20/01/2023”

--- src/App.js
+++ src/App.js
@@ -1,42 +1,59 @@
 import Transaction from "./Component/Transaction";
 import FormComponent from "./Component/FormComponent";
 import "./App.css"
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import DataContext from "./Data/DataContext";
 import ReportComponent from "./Component/ReportComponent";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 
 function App() {
   const design = { color: "red", textAlign: "center", fontSize: '1.5rem' }
-  const initState = [
-    {id:1,title:"เงินเดือน",amount:50000},
-    {id:1,title:"ค่าเดินทาง",amount:-1500},
-    {id:1,title:"ขายของ",amount:1200},
-    {id:1,title:"ค่ากิน",amount:-14000}
-  ]
   const [items, setItem] = useState([])//ชื่อ [state,Function setState] = useState(ค่าเริ่มต้นของ state)
   //ค่าเริ่มต้นอาจจะเป็นค่าเปล่า Arrary เปล่าได้ เช่น const [items,setItem] = useState([])
-  const [reportIncome,setReportIncome] = useState(0)
-  const [reportExpense,setReportExpense] = useState(0)
+  const [reportIncome, setReportIncome] = useState(0)
+  const [reportExpense, setReportExpense] = useState(0)
   const onAddNewItem = (newItem) => {//function รอรับค่าจาก FormComponent
     setItem((prevItem) => {
       return [newItem, ...prevItem]//ข้อมูลใหม่ ต่อข้อมูลเดิมหรือข้อมูลใหม่อยู่บนสุด
     })
   }
+
+  useEffect(() => {
+    const amounts = items.map(items => items.amount)
+    const income = amounts.filter(amounts => amounts > 0).reduce((total, amounts) => total += amounts, 0)
+    const expense = (amounts.filter(amounts => amounts < 0).reduce((total, amounts) => total += amounts, 0)) * -1
+    setReportIncome(income.toFixed(2))
+    setReportExpense(expense.toFixed(2))
+  }, [items, reportIncome, reportExpense])
   return (
-    <DataContext.Provider value = {
-      {
-        income:50000,
-        expense:-20000
-      }
+    <DataContext.Provider value={{ income: reportIncome, expense: reportExpense }
     }>
       <div className="Container">
         <h1 style={design}>โปรแกรมบัญชีรายรับ - รายจ่าย</h1>
-        <ReportComponent/>
-        <FormComponent onAddItem={onAddNewItem} />
-        <Transaction items={items} />
+        <Router>
+          <div>
+            <ul className="horizonntal-menu">
+              <li>
+                <Link to="/">ข้อมูลบัญชี</Link>
+              </li>
+              <li>
+                <Link to="/insert">บัญทึกข้อมูล</Link>
+              </li>
+            </ul>
+            <Routes>
+            <Route path='/' element={<ReportComponent />}></Route>
+            <Route path='/insert' element={<><FormComponent onAddItem={onAddNewItem} /> <Transaction items={items} /> </>}></Route>
+          </Routes>
+          </div>
+        </Router>
       </div>
     </DataContext.Provider>
   );
 }
+
+// <ReportComponent />
+// <FormComponent onAddItem={onAddNewItem} />
+// <Transaction items={items} />
 
 export default App;
--- src/Component/ReportComponent.js
+++ src/Component/ReportComponent.js
@@ -1,12 +1,26 @@
 import { useContext } from "react";
 import DataContext from "../Data/DataContext";
+import './ReportComponent.css'
 
 const ReportComponent = () => {
     const { income, expense } = useContext(DataContext)
+    const formatNumber=(num)=>{
+        return num.tostring().replace(/(\d)(?=(\d{3})+(?!\d))/g,'$1,')
+    }
     return (
         <div>
-            <p>รายรับ : {income}</p>
-            <p>รายจ่าย : {expense}</p>
+            <h4>ยอดคงเหลือ (บาท)</h4>
+            <h1>฿{(income - expense).toFixed(2)}</h1>
+            <div className="report-container">
+                <div>
+                    <h4>ยอดรายรับทั้งหมด</h4>
+                    <p className="report plus">฿{income}</p>
+                </div>
+                <div>
+                    <h4>ยอดรายจ่ายทั้งหมด</h4>
+                    <p className="report minus">฿{expense}</p>
+                </div>
+            </div>
         </div>
     );
 }
